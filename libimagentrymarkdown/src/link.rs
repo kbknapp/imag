4612080bd70800a5504b141e7c1b79a2fe829050
Matthias Beyer <mail@beyermatthias.de>
2016-05-24 16:30:06 +0200

    Implement link extractor

--- libimagentrymarkdown/src/link.rs
+++ libimagentrymarkdown/src/link.rs
@@ -1,8 +1,61 @@
 use result::Result;
 
-pub type Link = String;
+use hoedown::renderer::Render;
+use hoedown::Buffer;
+use hoedown::Markdown;
 
-pub fn extract_links(buf: &str) -> Result<Vec<Link>> {
-    unimplemented!()
+pub struct Link {
+    pub title: String,
+    pub link: String,
+}
+
+struct LinkExtractor {
+    links: Vec<Link>,
+}
+
+impl LinkExtractor {
+
+    pub fn new() -> LinkExtractor {
+        LinkExtractor { links: vec![] }
+    }
+
+    pub fn links(self) -> Vec<Link> {
+        self.links
+    }
+
+}
+
+impl Render for LinkExtractor {
+
+    fn link(&mut self,
+            _: &mut Buffer,
+            _: Option<&Buffer>,
+            link: Option<&Buffer>,
+            title: Option<&Buffer>)
+        -> bool
+    {
+        let link  = link.and_then(|l| l.to_str().ok()).map(String::from);
+        let title = title.and_then(|l| l.to_str().ok()).map(String::from);
+
+        match (link, title) {
+            (Some(link), Some(title)) => {
+                self.links.push(Link { link: link, title: title });
+                true
+            },
+
+            (a, b) => {
+                debug!("Cannot extract link from ({:?}, {:?})", a, b);
+                false
+            },
+        }
+
+    }
+
+}
+
+pub fn extract_links(buf: &str) -> Vec<Link> {
+    let mut le = LinkExtractor::new();
+    le.render(&Markdown::new(buf));
+    le.links()
 }
 
