91ce7f8f3b79e97ae55a109e2d7d84d1601cb66e
Matthias Beyer <mail@beyermatthias.de>
2016-05-29 11:17:05 +0200

    fixup! Add test: Two (similar) links

--- libimagentrymarkdown/src/link.rs
+++ libimagentrymarkdown/src/link.rs
@@ -81,10 +81,9 @@ mod test {
 
     #[test]
     fn test_two_similar_links() {
-        let testtext = r#"
-           Some [example text](http://example.com).
-           Some more [example text](http://example.com).
-        "#;
+        let testtext = format!("{}\n{}",
+                               "Some [example text](http://example.com).",
+                               "Some more [example text](http://example.com).");
 
         let exp = Link {
             title: String::from("example text"),
